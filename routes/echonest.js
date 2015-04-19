1e105697a3ed8d1677feaa8ec740f80538d1aa73
jam9608 <jmurray475@gmail.com>
2015-04-19 06:22:15 -0400

    page/endpoint for hotness query complete

--- routes/echonest.js
+++ routes/echonest.js
@@ -7,3 +7,23 @@ var hotness = require('../APIadaptors/hotnessfetcher');
 router.get('/', function (req, res, next) {
     res.send("ok");
 });
+
+router.get('/en', function (req, res, next) {
+    var artist=req.query['artist'];
+    var track=req.query['songname'];
+
+    hotness.hot(artist, track, function (err, data) {
+        if (!err) {
+            console.log(data);
+            return res.send({hotness:data});
+        }
+        else{
+            return res.send(err);
+        }
+    });
+
+
+
+});
+
+module.exports = router;
